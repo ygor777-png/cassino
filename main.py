953bdafcf86009ba9d142aef8c5cd19b27ae8e44
ygor777-png <ygor.souza1@outlook.com.br>
2026-02-23 18:35:12 -0300

    commit initial

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import time
 import random
 import os
+from collections import Counter
 
 class BagsBet:
     def __init__(self):
-        self.simbolos = ['💥', '⚔', '⚡', '🚀', '👽', '🔫']
+        self.simbolos = ['💥',
+                        '⚔',
+                        '⚡',
+                        '🚀',
+                        '👽',
+                        '🔫']
         self.saldo = 0
     
     #função limpar terminal
@@ -60,14 +66,28 @@ class BagsBet:
                 self.limpar()
                 print ('🚫 Opção inválida, por gentileza digite uma opção válida! 🚫')
 
+#função girar slots
+    #def girar(self):
+        #self.emojis = list(self.simbolos.keys())
+        #return random.choices(self.simbolos, k=3)
+    
+#função calcular o premio
+    #def calcular_premio(self, aposta, rolagem):
+        #cont = Counter (rolagem)
+
+        #soma apenas os repitodos
+        #mult_total = sum(self.simbolos[s] * qtd for s in cont.items() if qtd >= 2)
+
+        #return aposta * mult_total
+
     #função jogar
     def jogar(self):
         time.sleep (1)
-        print (f'SALDO R${self.saldo},00')
+        print (f'SALDO R${self.saldo:.2f}')
         while True:
-            self.texto_jogar = input (f'Quanto deseja apostar? (SALDO R${self.saldo},00)\n')
+            self.texto_jogar = input (f'Quanto deseja apostar? (SALDO R${self.saldo:.2f})\n')
             try:
-                self.numero_jogar = int(self.texto_jogar)
+                self.numero_jogar = float(self.texto_jogar)
             except ValueError:
                 print('🚫 Opção inválida, por gentileza digite uma opção válida! 🚫')
                 time.sleep(2)
@@ -87,24 +107,36 @@ class BagsBet:
                 print ('Girando slots 🤞🤞')
                 time.sleep (2)
                 self.limpar()
+
+                #rolagem das figuras 3x3
                 for i in range (5):
                     rolagem = [random.choice(self.simbolos) for i in range (3)]
+                    rolagem2 = [random.choice(self.simbolos) for i in range (3)]
+                    rolagem3 = [random.choice(self.simbolos) for i in range (3)]
                     print (' | '.join(rolagem))
+                    print (' | '.join(rolagem2))
+                    print (' | '.join(rolagem3))
                     time.sleep (0.1)
                     self.limpar()
                 
                 rolagem = [random.choice(self.simbolos) for i in range (3)]
+                rolagem2 = [random.choice(self.simbolos) for i in range (3)]
+                rolagem3 = [random.choice(self.simbolos) for i in range (3)]
                 print(' | '.join(rolagem))
+                print(' | '.join(rolagem2))
+                print(' | '.join(rolagem3))
 
+                #verifica se bateu todas as figuras iguais e recebe um mega ganho
                 if rolagem [0] == rolagem [1] == rolagem [2]:
                     self.ganho = self.apostado * 20
                     self.saldo += self.ganho
                     print('Parabéns, você deu um mega ganho!!🎊🎊')
-                    print (f'Premio {self.ganho},00!!')
-                    print(f'Saldo atualizado R${self.saldo},00')
+                    print (f'Premio {self.ganho:.2f}!!')
+                    print(f'Saldo atualizado R${self.saldo:.2f}')
                     print ('Deseja jogar novamente? [ 1 - SIM | 2 - NAO] ')
                     self.resposta = self.ler_numero_input()
                     
+                    #verifica se tem saldo pra continuar a apostar
                     while True:
                         if self.resposta == 1:
                             if self.saldo <= 0:
@@ -122,15 +154,17 @@ class BagsBet:
                             self.limpar()
                             self.jogar()
 
+                #verifica se teve duas figuras iguais e recebe um pequeno ganho
                 elif rolagem [0] == rolagem [1] or rolagem [0] == rolagem [2] or rolagem [1] == rolagem [2]:
                     self.ganho = self.apostado * 2
                     self.saldo += self.ganho
                     print ('Parabéns você ganhou um double! 🎉')
-                    print (f'Premio {self.ganho},00!!')
-                    print (f'Saldo atualizado R${self.saldo},00')
+                    print (f'Premio {self.ganho:.2f}!!')
+                    print (f'Saldo atualizado R${self.saldo:.2f}')
                     print ('Deseja jogar novamente? [ 1 - SIM | 2 - NAO] ')
                     self.resposta = self.ler_numero_input()
                     
+                    #verifica se ainda tem saldo pra continuar a apostar
                     while True:
                         if self.resposta == 1:
                             if self.saldo <= 0:
@@ -144,9 +178,11 @@ class BagsBet:
                             self.menu()
                         else:
                             print('🚫 Opção inválida, tente novamente! 🚫')
+                
+                #verifica se nao ganhar nada
                 else:
                     print('Que pena, nao ganhou nada! ❌❌')
-                    print (f'Saldo atualizado R${self.saldo},00')
+                    print (f'Saldo atualizado R${self.saldo:.2f}')
                     print ('Deseja jogar novamente? 🔁 [ 1 - SIM | 2 - NAO] ')
                     self.resposta = self.ler_numero_input()
                     
@@ -171,7 +207,7 @@ class BagsBet:
     
     #função consultar saldo
     def consultar_saldo(self):
-            print(f'Seu saldo atual é R${self.saldo},00')
+            print(f'Seu saldo atual é R${self.saldo:.2f}')
 
 
     #função depositar saldo
@@ -179,7 +215,7 @@ class BagsBet:
         while True:
             self.texto_depositar = (input (f'Quanto de saldo você deseja depositar? ').strip())
             try:
-                self.saldo_deposito = int(self.texto_depositar)
+                self.saldo_deposito = float(self.texto_depositar)
             except ValueError:
                 print('🚫 Opção inválida, tente novamente!🚫')
                 time.sleep(2)
@@ -189,12 +225,12 @@ class BagsBet:
 
             self.saldo += self.saldo_deposito
             self.limpar()
-            print (f'Saldo depositado, seu saldo atual é R${self.saldo}.')
+            print (f'Saldo depositado, seu saldo atual é R${self.saldo:.2f}.')
             time.sleep(1)
             while True:
                 print ('''[ 1 ] - RETORNAR AO MENU
 [ 2 ] - JOGAR''')
-                #self.texto = (input ('Oque deseja? '))
+                
                 self.resposta = self.ler_numero_input()
 
                 if self.resposta == 1:
@@ -211,9 +247,9 @@ class BagsBet:
     #função sacar saldo
     def sacar_saldo(self):
         while True:
-            self.texto_sacar = (input (f'Quanto de saldo você quer sacar? (SALDO R${self.saldo},00) '))
+            self.texto_sacar = (input (f'Quanto de saldo você quer sacar? (SALDO R${self.saldo:.2f}) '))
             try:
-                self.saldo_sacar = int(self.texto_sacar)
+                self.saldo_sacar = float(self.texto_sacar)
             except ValueError:
                 print('🚫 Opção inválida, tente novamente!🚫')
                 time.sleep(2)
@@ -227,7 +263,7 @@ class BagsBet:
                 self.limpar()
                 time.sleep (1)
                 print ('Saque realizado com sucesso ☑')
-                print (f'Saldo atualizado R${self.saldo},00')
+                print (f'Saldo atualizado R${self.saldo:.2f}')
             print ('''[ 1 ] - RETONAR AO MENU
 [ 2 ] - JOGAR''')
             
